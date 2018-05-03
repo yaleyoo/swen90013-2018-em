cb90747bf10a0c1be2b4923ceff597acb9ac43ae
Yuanyu Guo <yuanyug@student.unimelb.edu.au>
2018-05-03 11:03:19 +1000

    1. Create StaticValue class to hold static variables. 2. When visualization toggle is unticked, set renderer to be disabled.

--- Assets/Scripts/LeafGenerator.cs
+++ Assets/Scripts/LeafGenerator.cs
@@ -34,6 +34,9 @@ public class LeafGenerator : MonoBehaviour {
     // Dictionary of leaf shapes, and their ratio in the current leaves to be dropped
     private Dictionary<LeafShape, int> sizesAndRatios;
     private int totalRatioWeights;
+
+    // Is visualization?
+    private bool isVisualize;
     
     // Use this for initialization
     void Start () {
@@ -44,6 +47,7 @@ public class LeafGenerator : MonoBehaviour {
         this.stopAtLeafLimit = true;
         this.leafNumberLimit = 1000;
         this.listOfLeaves = new List<GameObject>();
+        this.isVisualize = StaticValue.GetIsVisualize(); // Set visualization for simulation
 
         // Default leaves defined here
         LeafShape AcaciaMelanoxylon = new LeafShape();
@@ -73,10 +77,9 @@ public class LeafGenerator : MonoBehaviour {
         // Automatically begin the simulation on start
         BeginSim(0.01f);
     }
-	
 
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update () {
 		
 	}
 
@@ -133,7 +136,13 @@ public class LeafGenerator : MonoBehaviour {
 
         // Rotate the leaf object randomly after it's spawn
         leafCopy.GetComponent<Leaf>().SetRotation(Random.Range(0f, 360f), Random.Range(0f, 360f), Random.Range(0f, 360f));
-        
+
+        // In case of no visualization, turn off the renderer for leaves
+        if (!this.isVisualize)
+        {
+            leafCopy.GetComponent<Renderer>().enabled = false;
+        }
+
         // Add the new leaf to the list of existing leaves
         this.listOfLeaves.Add(leafCopy);
         
---
+++ Assets/Scripts/StaticValue.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by Yuanyu Guo.
+ * Class to store static variables.
+ */
+
+public class StaticValue {
+    private static bool isVisualize;// Get visualization value from user input
+    //private static int leafLimitation;
+
+    // Public method to set isVisualize
+    public static void SetIsVisualize(bool isVisualize)
+    {
+        StaticValue.isVisualize = isVisualize;
+    }
+
+    // Public method to get isVisualize
+    public static bool GetIsVisualize()
+    {
+        return StaticValue.isVisualize;
+    }
+
+}

2981ffa8afc7ecf43d90a2745e76af1deded61de
Michael Lumley <mlumley@student.unimelb.edu.au>
2018-05-13 21:18:02 +1000

    Added unit test

--- Assets/Editor/LeafObjectTest.cs
+++ Assets/Editor/LeafObjectTest.cs
@@ -1,40 +1,7 @@
-﻿/** Created by Ze Wang 
- * Unit test for the leaf object
-**/
-using UnityEngine;
-using UnityEditor;
-using UnityEngine.TestTools;
-using NUnit.Framework;
-using System.Collections;
+﻿/** Created by Ze Wang  * Unit test for the leaf object**/using UnityEngine;using UnityEditor;using UnityEngine.TestTools;using NUnit.Framework;using System.Collections;public class LeafObjectTest {		[Test]	public void LeafSetAndGetName() {		//Arrage		GameObject gm = GameObject.Instantiate((GameObject)Resources.Load("Leaf"), new Vector3(0,10,0), Quaternion.Euler(0,0,0));		string leafName="";		//Act		gm.GetComponent<Leaf>().SetName("LeafName");		leafName = gm.GetComponent<Leaf> ().GetName ();		//Assert		Assert.AreEqual(leafName, "LeafName");	}	[Test]	public void LeafSetAndGetSize() {		GameObject gm = GameObject.Instantiate((GameObject)Resources.Load("Leaf"), new Vector3(0,10,0), Quaternion.Euler(0,0,0));		gm.GetComponent<Leaf> ().SetSize (0, 10, 0);		Vector3 v = new Vector3 (10, 0, 0);		Assert.AreEqual (v, gm.GetComponent<Leaf> ().GetSize ());	}    [Test]    public void LeafCheckIfMoving() {        GameObject gm = GameObject.Instantiate((GameObject)Resources.Load("Leaf"), new Vector3(0, 10, 0), Quaternion.Euler(0, 0, 0));        Leaf leaf = gm.GetComponent<Leaf>();        float speed = 0.5f;        float angularVelocity = 0.5f;        bool result = leaf.CheckIfMoving(speed, angularVelocity);        Assert.IsTrue(result);    }
 
-public class LeafObjectTest {
-	
-	[Test]
-	public void LeafSetAndGetName() {
-		//Arrage
-		GameObject gm = GameObject.Instantiate((GameObject)Resources.Load("Leaf"), new Vector3(0,10,0), Quaternion.Euler(0,0,0));
-		string leafName="";
-		//Act
-		gm.GetComponent<Leaf>().SetName("LeafName");
-		leafName = gm.GetComponent<Leaf> ().GetName ();
-		//Assert
-		Assert.AreEqual(leafName, "LeafName");
-	}
-	[Test]
-	public void LeafSetAndGetSize() {
-		GameObject gm = GameObject.Instantiate((GameObject)Resources.Load("Leaf"), new Vector3(0,10,0), Quaternion.Euler(0,0,0));
-		gm.GetComponent<Leaf> ().SetSize (0, 10, 0);
-		Vector3 v = new Vector3 (10, 0, 0);
-		Assert.AreEqual (v, gm.GetComponent<Leaf> ().GetSize ());
+    [Test]    public void LeafCheckIfNotMoving() {        GameObject gm = GameObject.Instantiate((GameObject)Resources.Load("Leaf"), new Vector3(0, 10, 0), Quaternion.Euler(0, 0, 0));        Leaf leaf = gm.GetComponent<Leaf>();        float speed = 0.4f;        float angularVelocity = 0.4f;        bool result = leaf.CheckIfMoving(speed, angularVelocity);        Assert.IsFalse(result);    }
 
-	}
-
-	// A UnityTest behaves like a coroutine in PlayMode
-	// and allows you to yield null to skip a frame in EditMode
-	[UnityTest]
-	public IEnumerator LeafObjectTestWithEnumeratorPasses() {
-		// Use the Assert class to test conditions.
-		// yield to skip a frame
-		yield return null;
-	}
-}
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    [UnityTest]	public IEnumerator LeafObjectTestWithEnumeratorPasses() {		// Use the Assert class to test conditions.		// yield to skip a frame		yield return null;	}}

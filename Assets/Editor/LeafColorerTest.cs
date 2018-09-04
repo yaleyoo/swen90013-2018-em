97be1f221f81fc3257fa3238f618580320e38aa3
Yudongg1 <y.gao64@student.unimelb.edu.au>
2018-09-04 11:48:16 +1000

    add random color test for leafColor

--- Assets/Editor/LeafColorerTest.cs
+++ Assets/Editor/LeafColorerTest.cs
@@ -17,17 +17,23 @@ public class LeafColorerTest {
 
 	LeafColorer lc = new LeafColorer();
 
+	/// <summary>
+	/// Color is assigned, and equals to the first color "Green".
+	/// </summary>
 	[Test]
 	public void ColorIsAssigned(){
-		List<Color> presetColor = lc.getPresetColors ();
+		//List<Color> presetColor = lc.getPresetColors ();
 
 		Color current_color = lc.GetColor (new LeafData());
 
 		Assert.IsTrue (current_color.Equals (Color.green));
 	}
 
+	/// <summary>
+	/// Color is removed from the presetColor list. 
+	/// </summary>
 	[Test]
-	public void ColorRemoved(){
+	public void ColorIsRemoved(){
 		
 		List<Color> presetColor = lc.getPresetColors ();
 
@@ -36,17 +42,43 @@ public class LeafColorerTest {
 		Assert.IsFalse (presetColor.Contains (removed_color));
 	}
 
+	/// <summary>
+	/// Run 10 rounds to use all colors in the presetColor list, 
+	/// the presetColor therefore should be empty.
+	/// </summary>
 	[Test]
 	public void presetColorEmpty(){
 		List<Color> presetColor = lc.getPresetColors ();
 
+		Color removed_color;
+
 		// run out of the colors in list
 		for (int i = 0; i < 9; i++){
-			Color removed_color = lc.GetColor (new LeafData("ALeaf", "Flat", i, i, i, i, i, i));
+			removed_color = lc.GetColor (new LeafData("ALeaf", "Flat", i, i, i, i, i, i));
 		}
 
 		Assert.IsTrue (presetColor.Count == 0);
+	}
 
+	/// <summary>
+	/// The generated random color shouldn't be the same with anyone in the preset list. 
+	/// </summary>
+	[Test]
+	public void randomColor(){
+		List<Color> usedColor = new List<Color> ();
+
+		Color removed_color;
+
+		for (int i = 0; i < 9; i++){
+			removed_color = lc.GetColor (new LeafData("ALeaf", "Flat", i, i, i, i, i, i));
+
+			usedColor.Add (removed_color);
+		}
+
+		// this is a random color, it shouldn't be included in 'usedColor' list
+		Color current_color = lc.GetColor (new LeafData());
+
+		Assert.IsFalse (usedColor.Contains(current_color));
 	}
 
 	// A UnityTest behaves like a coroutine in PlayMode

d0bf66aa920baa0f4636cd14c865d588e9b60159
Michael Lumley <mlumley@student.unimelb.edu.au>
2018-09-18 18:24:52 +1000

    Improvements to unit tests

--- Assets/Editor/DensityCalculationCylinderTest.cs
+++ Assets/Editor/DensityCalculationCylinderTest.cs
@@ -3,9 +3,10 @@ using NUnit.Framework;
 
 public class DensityCalculationCylinderTest {
 
-    // Test that the height of the cylinder is within two standard deviations of the mean height of leaves
-	[Test]
-	public void DensityCalculationCylinderCylinderHeightTest() {
+    DensityCalculationCylinder cylinder;
+
+    [OneTimeSetUp]
+    public void Init() {
         GameObject[] leaves = new GameObject[5];
 
         // Last leaf will be the highest
@@ -29,7 +30,13 @@ public class DensityCalculationCylinderTest {
             leaves[i] = leaf;
         }
 
-        DensityCalculationCylinder cylinder = new DensityCalculationCylinder(leaves, 100, 100);
+        cylinder = new DensityCalculationCylinder(leaves, 100, 100);
+    }
+
+    // Test that the height of the cylinder is within two standard deviations of the mean height of leaves
+	[Test]
+	public void DensityCalculationCylinderCylinderHeightTest() {
+
         float height = cylinder.ComputeCylinderHeightToUse();
 
         Assert.IsTrue(height >= 50 && height <= 52);
@@ -38,31 +45,6 @@ public class DensityCalculationCylinderTest {
     // Test that the random point is within the cylinder
     [Test]
     public void DensityCalculationCylinderRandomPointTest() {
-        GameObject[] leaves = new GameObject[5];
-
-        // Last leaf will be the highest
-        for (int i = 0; i < 5; i++) {
-            GameObject leaf = new GameObject();
-            leaf.transform.position = new Vector3(0, i * 10, 0);
-            leaf.AddComponent<Leaf>();
-
-            GameObject leftCollider = new GameObject();
-            leftCollider.name = "leftCollider";
-            leftCollider.AddComponent<BoxCollider>();
-            leftCollider.transform.parent = leaf.transform;
-            leftCollider.transform.localPosition = new Vector3(0, 0, -0.5f);
-
-            GameObject rightCollider = new GameObject();
-            rightCollider.name = "rightCollider";
-            rightCollider.AddComponent<BoxCollider>();
-            rightCollider.transform.parent = leaf.transform;
-            rightCollider.transform.localPosition = new Vector3(0, 0, 0.5f);
-
-            leaves[i] = leaf;
-        }
-
-        DensityCalculationCylinder cylinder = new DensityCalculationCylinder(leaves, 100, 100);
-
         Vector3 point = cylinder.RandomPointInCylinderSlice(5, 2);
 
         Assert.IsTrue(point.x >= -100 && point.x <= 100);
@@ -73,30 +55,6 @@ public class DensityCalculationCylinderTest {
     // Test that a point is in the object or not
     [Test]
     public void DensityCalculationCylinderPointInObjectsTest() {
-        GameObject[] leaves = new GameObject[5];
-
-        // Last leaf will be the highest
-        for (int i = 0; i < 5; i++) {
-            GameObject leaf = new GameObject();
-            leaf.transform.position = new Vector3(0, i * 10, 0);
-            leaf.AddComponent<Leaf>();
-
-            GameObject leftCollider = new GameObject();
-            leftCollider.name = "leftCollider";
-            leftCollider.AddComponent<BoxCollider>();
-            leftCollider.transform.parent = leaf.transform;
-            leftCollider.transform.localPosition = new Vector3(0, 0, -0.5f);
-
-            GameObject rightCollider = new GameObject();
-            rightCollider.name = "rightCollider";
-            rightCollider.AddComponent<BoxCollider>();
-            rightCollider.transform.parent = leaf.transform;
-            rightCollider.transform.localPosition = new Vector3(0, 0, 0.5f);
-
-            leaves[i] = leaf;
-        }
-
-        DensityCalculationCylinder cylinder = new DensityCalculationCylinder(leaves, 100, 100);
 
         // Test if point is in leaves
         Vector3 point = new Vector3(0, 20, 1);
